cc33c7050cdf99557145b3e10136ed83f9dac77a
lattermatter <116776079+lattermatter@users.noreply.github.com>
2024-06-21 16:42:01 -0700

    added time support

--- Dmg_Calc.py
+++ Dmg_Calc.py
@@ -21,7 +21,9 @@ mob_hp = {'Player': 20, 'Axolotl': 14, 'Bat': 6, 'Cat': 10, 'Chicken': 4, 'Cod':
 mob_armorPoints = {'Zombie': 2, 'Zombified Piglin': 2, 'Zombie Villager': 2, 'Husk': 2, 'Drowned': 2,
                    'Small Magma Cube': 3,
                    'Wither': 4, 'Medium Magma Cube': 6, 'Killer Rabbit': 8, 'Large Magma Cube': 12, 'Shulker': 20}
-mob = input("Enter mob attacked (Enter Q for options): ")
+mob = input("Enter mob attacked (Q for options, blank for player): ")
+
+
 counter = 0
 if mob == 'Q' or mob == 'q':
     print('Playable: ')
@@ -35,22 +37,25 @@ if mob == 'Q' or mob == 'q':
             print('\nHostile Mobs:')
         print(f'{counter}. {i}')
     mob = input("\nEnter mob attacked: ")
-armor_points = int(input("Enter the mob's armor points: "))
-armor_toughness = int(input("Enter the mob's armor toughness points: "))
 
-if mob in mob_hp:
+if mob == "" or mob == "Player":
+    mob = "Player"
+    hp = mob_hp[mob]
+    armor_points = int(input("Enter the mob's armor points: "))
+    armor_toughness = int(input("Enter the mob's armor toughness points (diamond adds 2 per piece, netherite adds 3 per piece): "))
+elif mob in mob_hp:
     hp = mob_hp[mob]
 else:
     print('Please a valid mob name.')
+
 if mob in mob_armorPoints and armor_points == 0 and armor_toughness == 0:
     armor_points = mob_armorPoints[mob]
     armor_toughness = 0
 
 # WEAPON
-weapon = input('Enter weapon used (e.g. Iron Sword, Golden Axe, etc.): ')
-weapon_type = ''
-weapon_material = ''
-index = 0
+weapon_type = input('Enter weapon used (e.g. Sword, Axe, Shovel, Pickaxe, Trident): ')
+weapon_material = input('Enter weapon used (e.g. Wooden, Gold, Iron, Diamond, Netherite): ')
+
 weapon_dmg = {'Sword': {'Wooden': 4, 'Golden': 4, 'Stone': 5, 'Iron': 6, 'Diamond': 7, 'Netherite': 8},
               'Axe': {'Wooden': 7, 'Golden': 7, 'Stone': 9, 'Iron': 9, 'Diamond': 9, 'Netherite': 10},
               'Shovel': {'Wooden': 2.5, 'Golden': 2.5, 'Stone': 3.5, 'Iron': 4.5, 'Diamond': 5.5, 'Netherite': 6.5},
@@ -62,20 +67,13 @@ weapon_dps = {'Sword': {'Wooden': 6.4, 'Golden': 6.4, 'Stone': 8, 'Iron': 9.6, '
               'Pickaxe': {'Wooden': 2.4, 'Golden': 2.4, 'Stone': 3.6, 'Iron': 4.8, 'Diamond': 6, 'Netherite': 7.2}
               }
 
-for letter in weapon:
-    index += 1
-    if letter == ' ':
-        break
-    weapon_material += letter
-for letter in weapon[index:]:
-    weapon_type += letter
-if weapon != 'Trident':
+if weapon_type != 'Trident':
     dmg = weapon_dmg[weapon_type][weapon_material]
     dps = weapon_dps[weapon_type][weapon_material]
 else:
     dmg = 9
     dps = 9.9
-dmg = float(input('Enter dmg: '))
+
 # CRITS
 crit = input('Crit? ')
 if crit == 'Yes' or crit == 'y' or crit == 'yes' or crit == 'Y':
@@ -94,9 +92,9 @@ hits = math.ceil(hp / dmg)
 total_dmg = hits * dmg
 
 print(
-    f'''To get a(n) {mob} ({hp} HP) with a(n) {weapon} {crit_Bool},
+    f'''To get a(n) {mob} ({hp} HP) with a(n) {weapon_material} {weapon_type} {crit_Bool},
 with {armor_points} defense points and {armor_toughness} toughness points, 
 it would take {hits} hits.
 Each hit would do {dmg} damage, amounting to {round(total_dmg, 2)} total damage.
-The total damage done would be {round(total_dmg - hp, 2)} more than the mob's HP ({hp}).
+The total damage done would be {round(total_dmg - hp, 2)} more than the mob's HP ({hp}) It would take at least {total_dmg/dps} seconds to finish.
 ''')
